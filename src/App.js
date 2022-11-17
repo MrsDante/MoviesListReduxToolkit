01e76327701629e26d56c1198065d45044f903ad
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-17 21:16:52 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import './App.css';
+import './App.scss';
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './components/Home/Home';
+import MovieDetails from './components/MovieDetails/MovieDetails';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Routes>
+        <Header></Header>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:imdbID" element={<MovieDetails />} />
+        <Route element={<PageNotFound />} />
+        <Footer />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+/*
+ <Routes>
+      <Route path="/" element={<Layout />}>
+
+        <Route index element={<PostsList />} />
+
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route exact path=":postId" element={<SinglePostPage />} />
+          <Route path="edit/:postId" element={<EditPostForm />} />
+        </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":indexId" element={<UserPage />} />
+        </Route>
+
+      // <Route path="*" element={<Navigate to="/" replace />} />
+     
+      // </Route>
+    // </Routes>
+//*/
---
+++ src/common/APIs/movieAPIKey.js
@@ -0,0 +1,1 @@
+export const APIKey = 'b835ed15';
