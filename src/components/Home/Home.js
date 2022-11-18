bc616d1e4a91bf839d9442e16857fe06a51b7929
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-18 18:52:04 +0300

    .

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,9 +8,15 @@ const Home = () => {
   useEffect(() => {
     const movieText = 'Harry';
     const fetchMovies = async () => {
-      const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`);
-    }
-  })
+      const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log('Err :', err);
+        });
+        console.log('The response from API', response);
+    };
+
+    fetchMovies();
+  }, []);
 
   return (
     <div>
