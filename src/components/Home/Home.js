e10cfae8d5b65a8b557f2f3e6d311602068b49b0
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-18 19:41:37 +0300

    .

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,16 +3,20 @@ import MovieListing from '../MovieListing/MovieListing';
 import movieApi from '../../common/APIs/movieAPI';
 //import { APIKey } from '../../common/APIs/MovieAPIKey';
 import { APIKey } from '../../common/APIs/movieAPIKey';
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/movieSlice';
 
 const Home = () => {
+  const movieText = 'Harry';
+  const dispatch = useDispatch();
+  
   useEffect(() => {
-    const movieText = 'Harry';
     const fetchMovies = async () => {
       const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log('Err :', err);
         });
-        console.log('The response from API', response);
+        dispatch(addMovies(response.data))
     };
 
     fetchMovies();
