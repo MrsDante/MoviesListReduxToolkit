e77a276d8cfbf63478283a374dcb5ee9be22724b
Анастасия <MrsDante@MacBook-Pro-Anastasia.local>
2022-11-18 20:17:50 +0300

    .

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './MovieCard.scss';
 
 const MovieCard = ({ data }) => {
   console.log(data)
   return (
-    <div>{data.Title}</div>
+    <div className="card-item">
+      <div className="catd-inner">
+        <div className="card-top">
+          <img src={ data.Poster } alt="MOVIE POSTERRRRRRRR" />
+        </div>
+        <div className="card-bottom">
+          <div className="card-info">
+            <h4>{ data.Title }</h4>
+            <p>{data.Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
